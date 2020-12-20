deaf746b85c14cda7bf419ec3315cae203753e86
sigeaki <ssiiggeeaakkii@g,ail.com>
2020-12-20 21:42:33 +0900

    20201220 change docomo_data.py

--- docomo_data.py
+++ docomo_data.py
@@ -2,9 +2,9 @@ import datetime
 import calendar
 n=float(input("今日までのデータ使用量は？:"))
 ddn=datetime.datetime.now()
-Day=ddn.day
-Mon=ddn.month
-N=n/Day
-_, ld = calendar.monthrange(ddn.year,Mon)
-print("{}日までの平均データ使用量は{:.2f}".format(Day,N))
-print("{}月のデータ使用量の予定は{:.2f}".format(Mon,N*ld))
+d=ddn.day
+m=ddn.month
+nn=n/d
+_, ld = calendar.monthrange(ddn.year,m)
+print("{}日までの平均データ使用量は{:.2f}".format(d,nn))
+print("{}月のデータ使用量の予定は{:.2f}".format(m,nn*ld))
