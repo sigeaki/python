ebfe0c6456bb934da6bcdf19cd432652967c0791
sigeaki <ssiiggeeaakkii@g,ail.com>
2021-04-03 22:47:29 +0900

    grep.py ver0.01

--- grep.py
+++ grep.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # coding: UTF-8
+# version 0.01
 import re
 import sys
 import datetime
 args = sys.argv
-if len(args) < 2:
+if len(args) < 3:
     ddn=datetime.datetime.now()
     search_word = ddn.strftime("-%m-%d")
 else:
-    search_word = args[1]
-with open('/home/sigeaki/LL750share/howm/diary.txt','r') as f:
+    search_word = args[-2]
+with open(args[-1],'r') as f:
     lines = f.readlines()
 lines_strip = [line.strip() for line in lines]
-#k = [line for line in lines_strip if search_word in line]
 k = [line for line in lines_strip if re.search(search_word,line)]
 for line in k:
     print(line)
-
-print('検索結果は{}回です'.format(len(k)))
+if args[1] == '-c':
+    print('マッチ行数は{}回です'.format(len(k)))
