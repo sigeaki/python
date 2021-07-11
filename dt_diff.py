600dead42121f2915f6dda725854d8d40fe65a18
sigeaki <ssiiggeeaakkii@g,ail.com>
2021-07-11 23:15:09 +0900

    change dt_diff.py

--- dt_diff.py
+++ dt_diff.py
@@ -3,17 +3,18 @@
 # version 0.01
 from datetime import date
 from monthdelta import monthmod
-dt = input("DAte?: ")
+dt = input("Date?: ")
 y, m, d = int(dt[:4]),int(dt[4:6]),int(dt[-2:])
 t1 = date.today()
 t2 = date(y,m,d)
 dt_dif = t1 - t2
-mmod = monthmod(t2,t1)
+mmod_1 = monthmod(t2,t1)
+mmod_2 = monthmod(t1,t2)
 print("{}日前です".format((dt_dif).days))
-if 1 <= mmod[0].months < 12:
-    print(f'{mmod[0].months}月 {mmod[1].days}日前です。')
-elif mmod[0].months >= 12:
-    y,m = divmod(mmod[0].months,12)
-    print(f'{y}年 {m}月 {mmod[1].days}日前です。')
+if 1 <= mmod_1[0].months < 12:
+    print(f'{mmod_1[0].months}月 {mmod_2[1].days}日前です。')
+elif mmod_1[0].months >= 12:
+    y,m = divmod(mmod_1[0].months,12)
+    print(f'{y}年 {m}月 {mmod_2[1].days}日前です。')
 else:
-    print(f'{mmod[1].days}日前です。')
+    print(f'{mmod_1[1].days}日前です。')
