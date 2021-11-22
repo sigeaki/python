12b2c855ab6e6bb6b76b1ee28a2056ae2b8f61f0
sigeaki <ssiiggeeaakkii@g,ail.com>
2021-11-22 21:33:53 +0900

    211122 change syouhizei.py

--- syouhizei.py
+++ syouhizei.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: UTF-8
-# version 0.04
+# version 0.05
 zeiritu = input("zeiritu?")
 if zeiritu == '8':
     z = 8 
@@ -12,7 +12,8 @@ def keisan(x):
     x = 1 + x / 100
     return x
 r = []
-while (a := input("price? ")) != '':
-    b = int(a)
-    r.append(b)
+a = input("price?:")
+while a != '':
+    r.append(int(a))
+    a = input("price?:")
 print(f'price: {r} Total: {sum(r):,} TaxTotal: {int(sum(r) * keisan(z)):,}')
