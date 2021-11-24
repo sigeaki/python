20a619f306382346be74c34cf5ce1a3ba1906868
sigeaki <ssiiggeeaakkii@g,ail.com>
2021-11-24 22:17:06 +0900

    211124 change syouhizei.py

--- kikan_002.py
+++ kikan_002.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 # coding: UTF-8
-# version 0.03
-import sys
-import datetime
+# version 0.01
+from datetime import date
 from monthdelta import monthmod
-args = sys.argv
-with open(args[-1],'r') as f:
-    lines = f.readlines()
-lines_strip = [line.strip() for line in lines]
-k = [line for line in lines_strip if args[-2] in line]
-k1 = k[-1][1:11]
-k2 = k[-2][1:11]
-t1 = datetime.date.fromisoformat(k1)
-t2 = datetime.date.fromisoformat(k2)
-dt_dif = t1 - t2
-mmod = monthmod(t2,t1)
+dt = input("DAte?: ")
+y, m, d = int(dt[:4]),int(dt[4:6]),int(dt[-2:])
+t1 = date(y,m,d)
+t2 = date.today()
+dt_dif = t2 - t1
+mmod = monthmod(t1,t2)
 print("{}日前です".format((dt_dif).days))
 if 1 <= mmod[0].months < 12:
     print(f'{mmod[0].months}月 {mmod[1].days}日前です。')
@@ -23,5 +17,3 @@ elif mmod[0].months >= 12:
     print(f'{y}年 {m}月 {mmod[1].days}日前です。')
 else:
     print(f'{mmod[1].days}日前です。')
-print(k[-2])
-print(k[-1])
--- syouhizei.py
+++ syouhizei.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: UTF-8
-# version 0.05
-zeiritu = input("zeiritu?")
+# version 0.06
+zeiritu = input("zeiritu? ")
 if zeiritu == '8':
     z = 8 
 elif zeiritu == '10':
@@ -9,11 +9,12 @@ elif zeiritu == '10':
 else:
     z = 0
 def keisan(x):
-    x = 1 + x / 100
-    return x
+    return 1 + x / 100
 r = []
-a = input("price?:")
+a = input("price?: ")
 while a != '':
     r.append(int(a))
-    a = input("price?:")
+    a = input("price?: ")
+if z == 0:
+    print(f'price: {r} Total: {sum(r):,}')
 print(f'price: {r} Total: {sum(r):,} TaxTotal: {int(sum(r) * keisan(z)):,}')
