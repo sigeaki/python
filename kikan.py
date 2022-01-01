40d245393a94ffb8856d1f8a0af873c6ceb042be
sigeaki <ssiiggeeaakkii@g,ail.com>
2022-01-01 21:08:19 +0900

    220101 change kikan.py

--- kikan.py
+++ kikan.py
@@ -21,23 +21,24 @@ mmod2 = monthmod(t2, t1)
 mmod3 = monthmod(t3, t2)
 if t1 == ddn:
     print(f'{args[-2]}は今日です。')
+    print(f'前回は{t2}です')
     if 1 <= mmod2[0].months < 12:
-        print(f'{args[-2]}は前回の{(t2 - t3).days}日、{mmod2[0].months:2}月{mmod2[1].days:2}日前です。')
+        print(f'前々回は{t3} 前回の{(t2 - t3).days}日 {mmod2[0].months:2}月{mmod2[1].days:2}日前です。')
     elif mmod2[0].months >= 12:
         y, m = divmod(mmod2[0].months, 12)
-        print(f'{args[-2]}は前回の{(t2 - t3).days}日。{y:2}年{m:2}月{mmod2[1].days:2}日前です。')
+        print(f'前々回は{t3} 前回の{(t2 - t3).days}日 {y:2}年{m:2}月{mmod2[1].days:2}日前です。')
     else:
-        print(f'{args[-2]}は前回の{(t2 - t3).days}日前です')
+        print(f'前々回は{t3} 前回の{(t2 - t3).days}日前です')
 else:
     if 1 <= mmod1[0].months < 12:
-        print(f'{args[-2]}は今日から{(ddn - t1).days}日、{mmod1[0].months:2}月{mmod1[1].days:2}日前です。')
+        print(f'{args[-2]}は今日から{(ddn - t1).days}日 {mmod1[0].months:2}月{mmod1[1].days:2}日前です。')
     elif mmod1[0].months >= 12:
         y, m = divmod(mmod1[0].months, 12)
-        print(f'{args[-2]}は今日から{(ddn - t1).days}日、{y:2}年{m:2}月{mmod1[1].days:2}日前です。')
+        print(f'{args[-2]}は今日から{(ddn - t1).days}日 {y:2}年{m:2}月{mmod1[1].days:2}日前です。')
     else:
         print(f'{args[-2]}は今日から{(ddn - t1).days}日前です')
     if 1 <= mmod3[0].months < 12:
-        print(f'{args[-2]}は前回から{(t1 - t2).days}日、{mmod3[0].months:2}月{mmod3[1].days:2}日前です。')
+        print(f'{args[-2]}は前回から{(t1 - t2).days}日 {mmod3[0].months:2}月{mmod3[1].days:2}日前です。')
     elif mmod3[0].months >= 12:
         y, m = divmod(mmod3[0].months, 12)
         print(f'{args[-2]}は前回から{(t1 - t2).days}日。{y:2}年{m:2}月{mmod3[1].days:2}日前です。')
