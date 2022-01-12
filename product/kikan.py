4463548a0d000a6f1f47d00c2222ccf04a2629c8
sigeaki <ssiiggeeaakkii@g,ail.com>
2022-01-12 23:30:13 +0900

    220112 change kikan.py

--- product/kikan.py
+++ product/kikan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: UTF-8
-# version 0.05
+# version 0.06
 import sys
 import datetime
 from monthdelta import monthmod
@@ -21,7 +21,14 @@ mmod2 = monthmod(t2, t1)
 mmod3 = monthmod(t3, t2)
 if t1 == ddn:
     print(f'{args[-2]}は今日です。')
-    print(f'前回は{t2}です')
+    # print(f'前回は{t2}です')
+    if 1 <= mmod1[0].months < 12:
+        print(f'前回は今日から{(t1 - t2).days}日 {mmod1[0].months:2}月{mmod1[1].days:2}日前です。')
+    elif mmod1[0].months >= 12:
+        y, m = divmod(mmod1[0].months, 12)
+        print(f'前回は今日から{(t1 - t2).days}日 {y:2}年{m:2}月{mmod1[1].days:2}日前です。')
+    else:
+        print(f'前回は{t2} 前回の{(t1 - t2).days}日前です')
     if 1 <= mmod2[0].months < 12:
         print(f'前々回は{t3} 前回の{(t2 - t3).days}日 {mmod2[0].months:2}月{mmod2[1].days:2}日前です。')
     elif mmod2[0].months >= 12:
@@ -38,11 +45,11 @@ else:
     else:
         print(f'{args[-2]}は今日から{(ddn - t1).days}日前です')
     if 1 <= mmod3[0].months < 12:
-        print(f'{args[-2]}は前回から{(t1 - t2).days}日 {mmod3[0].months:2}月{mmod3[1].days:2}日前です。')
+        print(f'{args[-2]}は前回から{(t2 - t3).days}日 {mmod3[0].months:2}月{mmod3[1].days:2}日前です。')
     elif mmod3[0].months >= 12:
         y, m = divmod(mmod3[0].months, 12)
-        print(f'{args[-2]}は前回から{(t1 - t2).days}日。{y:2}年{m:2}月{mmod3[1].days:2}日前です。')
+        print(f'{args[-2]}は前回から{(t2 - t3).days}日。{y:2}年{m:2}月{mmod3[1].days:2}日前です。')
     else:
-        print(f'{args[-2]}は前回から{(t1 - t2).days}日前です')
+        print(f'{args[-2]}は前回から{(t2 - t3).days}日前です')
 for i in range(-1,-4,-1):
     print(k[i])
