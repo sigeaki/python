8c6f78ace5fc1330ccbc79ee3169f239d636eade
sigeaki <ssiiggeeaakkii@g,ail.com>
2022-02-12 19:36:42 +0900

    220212-2 change kikan.py

--- product/kikan.py
+++ product/kikan.py
@@ -21,7 +21,7 @@ mmod2 = monthmod(t2, t1)
 mmod3 = monthmod(t3, t2)
 if t1 == ddn:
     print(f'{args[-2]}は今日です。')
-    print(mmod2[0].months)
+    # print(mmod2[0].months)
     if 1 <= mmod2[0].months < 12:
         print(f'前回は今日から{(t1 - t2).days}日 {mmod2[0].months:2}月{mmod2[1].days:2}日前です。')
     elif mmod2[0].months >= 12:
