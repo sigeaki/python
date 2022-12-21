b237784c48dca5b0cdd4e2093800bf467f70d7d3
sigeaki <ssiiggeeaakkii@g,ail.com>
2022-12-21 22:59:17 +0900

    20221221 change shogi_query.py

--- shogi_query.py
+++ shogi_query.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# version 0.01
+# version 0.02
 
 import sys
 import pandas as pd
@@ -9,8 +9,10 @@ args = sys.argv
 jyoken = input('抽出条件を入力してください:')
 s_jyoken = input('ソート条件を入力してください:')
 df = pd.read_csv(args[1],encoding='utf_8_sig',index_col=False)
-for i in '勝数','負数':
-    df[i] = df[i].dropna()#.apply(int).apply(str)
+df = df.fillna(0)
+df = df.astype({'勝数': 'int64', '負数': 'int64'})
+# for i in '勝数','負数':
+#     df[i] = df[i].dropna()#.apply(int).apply(str)
 df_data = df.query(jyoken)
 df_data.reset_index(drop=True, inplace=True)
 d  = pd.DataFrame(df_data)
