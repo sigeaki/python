2333f2682ecf09c9a5f8a4b618177fea8a2ed149
sigeaki <ssiiggeeaakkii@gmail.com>
2023-03-02 01:58:49 +0900

    20230302 change get_tempreture.py

--- product/get_temperature.py
+++ product/get_temperature.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# version 0.02
+# version 0.03
 
 import requests
 import json
@@ -65,9 +65,9 @@ row = dt_str_list[1:] + df_data1 + df_data2
 # print(fields)
 # print(row)
 hd = os.path.expanduser("~")
-print(Path(hd + "/" + dt_str_list[0] + 'temp.csv').exists())
+# print(Path(hd + "/" + dt_str_list[0] + 'temp.csv').exists())
 #print(fl) 
-if not Path(hd + "/"  + dt_str_list[0] + 'weather.csv').exists():
+if Path(hd + "/"  + dt_str_list[0] + 'weather.csv').exists():
     with open(hd + "/" + dt_str_list[0] + 'temp.csv', 'w', newline='', encoding='utf_8_sig') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
