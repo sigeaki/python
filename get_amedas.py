028a79373318abe2ac7442d6f1f7a37f3466948e
sigeaki <ssiiggeeaakkii@gmail.com>
2025-06-23 21:08:09 +0900

    20250623 changed get_amedas.py

--- get_amedas.py
+++ get_amedas.py
@@ -1,76 +1,27 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# version 0.11
+# version 0.12
 
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-import datetime
 import numpy as np
+import datetime
 import sys
 
 args = sys.argv
 dt_now = datetime.datetime.now()
+dt_now_str_1 = f'{dt_now.year}年{dt_now.month}月{dt_now.day}日'
+t = int(dt_now.hour) + 1
+print(dt_now_str_1)
 
-response = requests.get('https://weather.yahoo.co.jp/weather/amedas/1b/14136.html')
-mat = []  # 保存先の行列
-soup = BeautifulSoup(response.content, "html.parser")
-# tableの取得
-table = soup.find('table')
-
-# trの解析
-trs = table.find_all('tr')
-# ヘッダーの解析
-r = []  # 保存先の行
-tr = trs[0]
-for td in tr.find_all('td'):  # 1回目のtdタグを走査する
-    r.append(td.text.strip())
-mat.append(r[:-1])
-
-# ボディの解析
-for tr in trs[1:]:  # 最初の行を飛ばしてfor文で回す
-    r = []  # 保存先の行
-    for td in tr.find_all('td'):  # tdタグを走査する
-        r.append(td.text.strip())
-    mat.append(r[:-1])
-dt_nor_str_1 = dt_now.strftime('%Y-%m-%d')
-dt_nor_str_2 = f'{dt_now.year}年{dt_now.month}月{dt_now.day}日'
-
-l = dt_now.hour + 2
-
-m = []
-for k in mat[:-1]:
-    if len(k) > 6 and k[1] == '24時':
-        k[1] = '0時'
-        m.append(k[1:])
-    if len(k) > 6:
-        m.append(k[1:])
-    else:
-        m.append(k)
-df = pd.DataFrame(data=m[1:l], columns=m[0])
-try:
-    df_int = df.astype({'気温（℃）':'float64','降水量（mm）':'float64','風速（m/s）':'float64','日照時間（分）':'int8'})
-except:
-    df.replace('---', np.NaN, inplace=True)
-    # df_int = df.astype({'気温（℃）':'float64','降水量（mm）':'float64','風速（m/s）':'float64','日照時間（分）':'int8'})
-    df_int = df.astype({'気温（℃）':'float64','降水量（mm）':'float64','風速（m/s）':'float64','日照時間（分）':'float64'})
-# hd = os.path.expanduser("~")
-
-# if df_int.iloc[0,0] == '23時':
-#     df_int.iloc[::-1].to_csv(hd + '/' + dt_nor_str_1 + "weather.csv",index = False,encoding='utf_8_sig')
-# else:
-df_temp_avg = df_int['気温（℃）'].mean()
-df_rain_total = df_int['降水量（mm）'].sum()
-df_wind_avg = df_int['風速（m/s）'].mean()
-df_sun_total = df_int['日照時間（分）'].sum()
-# print(df_int.iloc[::-1])
-print(df_int)
-print(f'{dt_nor_str_2}の平均気温は、{df_temp_avg:.1f}℃')
-print(f'{dt_nor_str_2}の合計降水量は、{df_rain_total:,}mm')
-print(f'{dt_nor_str_2}の平均風速は、{df_wind_avg:.1f}（m/s）')
-print(f'{dt_nor_str_2}の合計日照時間は、{df_sun_total:,}分')
+response = pd.read_html('https://tenki.jp/amedas/1/2/14136.html', encoding='utf-8', index_col=1)
+pd.options.display.float_format = '{:.1f}'.format
+# df = response[4].iloc[:-1,:6]
+df = response[4].iloc[:t,1:6]
+df = df.astype({'気温(℃)':'float','降水量(mm)':'float32','風速(m/s)':'float32','日照時間(分)':'int8'})
+print(df.describe()) # describe関数でとりあえず概要表示
+print(df)
 if len(args) > 1 and args[1] == "-g":
     import matplotlib.pyplot as plt
     import japanize_matplotlib
-    df_int.plot(x='時刻', y=['気温（℃）','降水量（mm）','日照時間（分）','風速（m/s）'], title=dt_nor_str_2, marker="o", markersize=4, grid=True)
+    df.plot(y=['気温(℃)','風速(m/s)'], marker="o", markersize=4, grid=True)
     plt.show()
